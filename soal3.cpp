99f7ff92cba666c07259073927a0dc729b81c44f
eddyyucca <eddyyucca@gmail.com>
2017-11-07 05:16:01 -0800

    untuk menampilkan dan memasukkan nilai

---
+++ soal3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+int main () {
+	int gp,jk,lembur,gt,pajak;
+	cout << "masukkan gaji pwgawai : ";
+	cin >> gp;
+	cout << "masukkan jam kerja : ";
+	cin >> jk;
+	 
+}
