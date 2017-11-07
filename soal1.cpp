1bc754e8bd26b218db572a135f5f28e0927a8e7f
eddyyucca <eddyyucca@gmail.com>
2017-11-07 04:09:37 -0800

    proses menampilkan text inputan yg dipilih

---
+++ soal1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+using namespace std;
+
+int main () {
+
+	int x, y, z;
+	cout << "input nilai x = ";
+	cin >> x;
+
+	cout << "input nilai y =";
+	cin >> y;
+}
